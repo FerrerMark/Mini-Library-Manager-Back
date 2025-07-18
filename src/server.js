5bc57d9c8bf545710f78ade58028be635405d067
FerrerMark <ferrerjohnmark26@gmail.com>
2025-07-18 23:09:14 +0800

    first commit

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import { connectDB } from "./config/db.js";
+import bookRouter from "./routes/bookRoute.js";
+import userRouter from "./routes/userRoute.js"; 
+import { verifyToken, authorize } from "./middleware/authMiddleware.js";
+import logRouter from "./routes/logRoute.js";
+
+dotenv.config();
+await connectDB();
+
+const app = express();
+
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST", "DELETE", "PUT"]
+};
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+
+
+app.use("/api/books", bookRouter);
+app.use("/api/users", userRouter);
+app.use("/", logRouter);
+
+// Test route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(8080, () => console.log("Server running on port 8080"));
