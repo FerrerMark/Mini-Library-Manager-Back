bf2a74cc8e7fc303d3ef13bcaf29d0e935efd2ba
FerrerMark <ferrerjohnmark26@gmail.com>
2025-07-19 14:16:31 +0800

    cors

--- src/server.js
+++ src/server.js
@@ -13,7 +13,8 @@ await connectDB();
 const app = express();
 
 const allowedOrigins = [
-  'https://mini-library-manager.onrender.com/'
+  'https://mini-library-manager.onrender.com',
+  'https://mini-library-manager.onrender.com/',
 ];
 
 app.use(cors({
